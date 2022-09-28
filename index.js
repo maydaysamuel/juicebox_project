f17860b7553209bb8a2dbbe5fdc6437962ea880a
Samuel <samuel.koch08@gmail.com>
2022-09-27 18:14:51 -0600

    juicebox part 2 part one

---
+++ index.js
@@ -0,0 +1,27 @@
+const PORT = 3000;
+const express = require('express');
+const server = express();
+
+const apiRouter = require('./api')
+server.use('/api', apiRouter)
+
+const morgan = require('morgan')
+server.use(morgan('dev'));
+
+server.use(express.json());
+
+server.use((req, res, next) => {
+    console.log('<--- Body Logger START --->');
+    console.log(req.body);
+    console.log('<--- Body Logger END --->');
+
+    
+    next();
+})
+
+const { client } = require('./db');
+client.connect();
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
